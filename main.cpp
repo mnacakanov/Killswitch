9b931955855e6e21a3d5fd4df52966c2c0a297fb
mnacakanov <petrushksa@gmail.com>
2017-09-26 08:52:03 +0300

    Updated main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
-//#include "wmiapp.h"
 #include <QApplication>
 #include <QString>
 #include <QDebug>
-#include <QTest>
 #include "application.h"
+
 QString convertProperty(QAxObject* pObject, const char* pName)
 {
     return pObject->dynamicCall(pName).toString();
@@ -22,25 +21,4 @@ int main(int argc, char *argv[])
     {
         return 0;
     }
-
-//    Controller* controller = new Controller();
-
-//    controller->mainCycle();
-//    WMIapp WMIapplication;
-//    WMIapplication.initNetworkAdapterInterfacing();
-//    QVector<QAxObject*> networkAdapters;
-//        QTest::qWait(1000);
-//    int numInterfaces = WMIapplication.getConnectedNetworkAdapterObj(networkAdapters);
-//    qDebug() << "Got " << networkAdapters.size() << " interfaces";
-//    for(int i = 0; i < networkAdapters.size(); i++)
-//    {
-//        WMIapplication.executeMethod(networkAdapters[i], "Disable");
-//    }
-//    QTest::qWait(10000);
-//    for(int i = 0; i < networkAdapters.size(); i++)
-//    {
-//        WMIapplication.executeMethod(networkAdapters[i], "Enable");
-//    }
-
-
 }
